e9458e2bf76f337951a1ae25df2838317cfa122c
Roc136 <1361050885@qq.com>
2020-06-18 13:42:32 +0800

    Site updated: 2020-06-18 13:42:26

--- js/src/text.js
+++ js/src/text.js
@@ -1,16 +1,17 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
   $("body").click(function(e) {
-    var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+    var a = new Array("♥富强♥", "♥民主♥", "♥文明♥", "♥和谐♥", "♥自由♥", "♥平等♥", "♥公正♥", "♥法治♥", "♥爱国♥", "♥敬业♥", "♥诚信♥", "♥友善♥");
     var $i = $("<span/>").text(a[a_idx]);
     var x = e.pageX,
     y = e.pageY;
+	var c = gc();
     $i.css({
       "z-index": 99999,
       "top": y - 28,
       "left": x - a[a_idx].length * 8,
       "position": "absolute",
-      "color": "#ff7a45"
+      "color": c
     });
     $("body").append($i);
     $i.animate({
@@ -20,5 +21,7 @@ jQuery(document).ready(function($) {
       $i.remove();
     });
     a_idx = (a_idx + 1) % a.length;
+	c = gc();
   });
 });
+function gc(){var r=Math.ceil(Math.random()*256).toString(16);var g=Math.ceil(Math.random()*256).toString(16);var b=Math.ceil(Math.random() * 256).toString(16);return "#"+r+g+b}
