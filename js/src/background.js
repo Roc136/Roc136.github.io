1f70409807fd6d21d48753d0a619d31286b2c3a7
Roc136 <1361050885@qq.com>
2020-07-01 23:52:00 +0800

    Site updated: 2020-07-01 23:51:45

--- js/src/background.js
+++ js/src/background.js
@@ -1,4 +1,4 @@
-var bg_num=6,time=600000,bg_id=(Math.ceil(new Date().getTime() / time) % bg_num + 1).toString();
+var bg_num=11,time=600000,bg_id=(Math.ceil(new Date().getTime() / time) % bg_num + 1).toString();
 // var url="https://blog-roc.oss-cn-hongkong.aliyuncs.com/background/bj" + bg_id + ".jpg";
 var url="/images/background/bj" + bg_id + ".jpg";
 document.documentElement.style.setProperty('--bgurl', "url("+url+")");
