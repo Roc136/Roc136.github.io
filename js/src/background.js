7630955c1526dd8a5182021607f70751c25d0e75
Roc136 <1361050885@qq.com>
2020-07-16 13:24:48 +0800

    Site updated: 2020-07-16 13:24:18

--- js/src/background.js
+++ js/src/background.js
@@ -1,5 +1,9 @@
-var bg_num=31,time=600000,bg_id=(Math.ceil(new Date().getTime() / time) % bg_num + 1).toString();
-// var url="https://blog-roc.oss-cn-hongkong.aliyuncs.com/background/bj" + bg_id + ".jpg";
+var time=600000;
+if (window.innerWidth < 768) {
+    var bg_num=6,bg_id="m" + (Math.ceil(new Date().getTime() / time) % bg_num + 1).toString(); //手机端背景
+} else {
+    var bg_num=31,bg_id=(Math.ceil(new Date().getTime() / time) % bg_num + 1).toString(); //电脑端背景
+}
 var url="/images/background/bj" + bg_id + ".jpg";
 document.documentElement.style.setProperty('--bgurl', "url("+url+")");
 console.log("背景id（每"+time/60000+"分钟改变一次）："+bg_id);
