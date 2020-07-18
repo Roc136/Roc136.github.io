22359f51103cf6d533f752d4f804b9a2c939a241
Roc136 <1361050885@qq.com>
2020-07-18 16:17:48 +0800

    Site updated: 2020-07-18 16:17:36

---
+++ js/src/link_num.js
@@ -0,0 +1,13 @@
+link = {
+    init: function() {
+        //这里设置的是刚才的 linklist.json 文件路径
+        $.getJSON("/links/linklist.json",
+        function(data) {
+            var num = document.createElement("div");
+            num.innerHTML = data.length;
+            var links = document.getElementById("links-count")
+            links.appendChild(num);
+        });
+    }
+  }
+  link.init();
