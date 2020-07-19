6369fe620ebf3cd71d0dc105c1b52e1bb58379d7
Roc136 <1361050885@qq.com>
2020-07-19 18:55:44 +0800

    Site updated: 2020-07-19 18:55:40

--- js/src/background.js
+++ js/src/background.js
@@ -1,1 +1,14 @@
-"use strict";var bg_id,time=6e5,bg_num=31,bgm_num=6,url="/images/background/bj"+(bg_id=window.innerWidth<768?"m"+(Math.ceil((new Date).getTime()/time)%bgm_num+1).toString():(Math.ceil((new Date).getTime()/time)%bg_num+1).toString())+".jpg";document.documentElement.style.setProperty("--bgurl","url("+url+")"),console.log("背景id（每"+time/6e4+"分钟改变一次）："+bg_id);var num=document.createElement("div");num.innerHTML=bg_num+bgm_num;var photo=document.getElementById("photo-count");photo.appendChild(num);
+var time=600000,bg_num = 31, bgm_num = 6;
+if (window.innerWidth < 768) {
+    var bg_id="m" + (Math.ceil(new Date().getTime() / time) % bgm_num + 1).toString(); //手机端背景
+} else {
+    var bg_id=(Math.ceil(new Date().getTime() / time) % bg_num + 1).toString(); //电脑端背景
+}
+var url="/images/background/bj" + bg_id + ".jpg";
+document.documentElement.style.setProperty('--bgurl', "url("+url+")");
+console.log("背景id（每"+time/60000+"分钟改变一次）："+bg_id);
+
+var num = document.createElement("div");
+num.innerHTML = bg_num + bgm_num;
+var photo = document.getElementById("photo-count")
+photo.appendChild(num);
--- js/src/photo.js
+++ js/src/photo.js
@@ -1,1 +1,58 @@
-"use strict";var imageWidth,imgDataPath="/images/photoslist.json",imgPath="/images/background/",imgMaxNum=1e3,windowWidth=window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;imageWidth=windowWidth<768?175:310,photo={page:1,offset:imgMaxNum,init:function(){var t=this;$.getJSON(imgDataPath,function(i){t.render(t.page,i)})},render:function(i,t){var a=(i-1)*this.offset,e=i*this.offset;if(!(a>=t.length)){for(var n,d,o,g,m,r="",h=a;h<e&&h<t.length;h++)d=(n=t[h].split(" ")[1]).split(".")[0],g=(o=t[h].split(" ")[0]).split(".")[0],m=o.split(".")[1],r+='<div class="card" style="width:'+imageWidth+'px" ><div class="ImageInCard" style="height:'+imageWidth*m/g+'px"><a data-fancybox="gallery" href="'+imgPath+n+'" data-caption="'+d+'" title="'+d+'"><img data-src="'+imgPath+n+' " src="'+imgPath+n+' " data-loaded="true"></a></div></div>';$(".ImageGrid").append(r),this.minigrid()}},minigrid:function(){var i=new Minigrid({container:".ImageGrid",item:".card",gutter:12});i.mount(),$(window).resize(function(){i.mount()})}},photo.init();
+var imgDataPath = '/images/photoslist.json'; //图片名称高宽信息json文件路径
+var imgPath = '/images/background/';  //图片访问路径
+var imgMaxNum = 1000; //图片显示数量
+
+var windowWidth = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+if (windowWidth < 768) {
+    var imageWidth = 175; //图片显示宽度(手机端)
+} else {
+    var imageWidth = 310; //图片显示宽度
+}
+
+photo = {
+    page: 1,
+    offset: imgMaxNum,
+    init: function () {
+        var that = this;
+        $.getJSON(imgDataPath, function (data) {
+            that.render(that.page, data);
+            //that.scroll(data);
+        });
+    },
+    render: function (page, data) {
+        var begin = (page - 1) * this.offset;
+        var end = page * this.offset;
+        if (begin >= data.length) return;
+        var html, imgNameWithPattern, imgName, imageSize, imageX, imageY, li = "";
+        for (var i = begin; i < end && i < data.length; i++) {
+           imgNameWithPattern = data[i].split(' ')[1];
+           imgName = imgNameWithPattern.split('.')[0]
+           imageSize = data[i].split(' ')[0];
+           imageX = imageSize.split('.')[0];
+           imageY = imageSize.split('.')[1];
+            li += '<div class="card" style="width:' + imageWidth + 'px" >' +
+                    '<div class="ImageInCard" style="height:'+ imageWidth * imageY / imageX + 'px">' +
+                      '<a data-fancybox="gallery" href="' + imgPath + imgNameWithPattern + '" data-caption="' + imgName + '" title="' +  imgName + '">' +
+                        '<img data-src="' + imgPath + imgNameWithPattern + ' " src="' + imgPath + imgNameWithPattern + ' " data-loaded="true">' +
+                      '</a>' +
+                    '</div>' +
+                  '</div>'
+        }
+        $(".ImageGrid").append(li);
+        this.minigrid();
+    },
+    minigrid: function() {
+        var grid = new Minigrid({
+            container: '.ImageGrid',
+            item: '.card',
+            gutter: 12
+        });
+        grid.mount();
+        $(window).resize(function() {
+           grid.mount();
+        });
+    }
+}
+photo.init();
