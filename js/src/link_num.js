c7a7aa86eca3e11ec884f293bdc9671c04a42e8f
Roc136 <1361050885@qq.com>
2020-07-19 21:24:15 +0800

    Site updated: 2020-07-19 21:24:11

--- js/src/link_num.js
+++ js/src/link_num.js
@@ -1,13 +1,1 @@
-var link = {
-    init: function() {
-        //这里设置的是刚才的 linklist.json 文件路径
-        $.getJSON("/links/linklist.json",
-        function(data) {
-            var num = document.createElement("div");
-            num.innerHTML = data.length;
-            var links = document.getElementById("links-count")
-            links.appendChild(num);
-        });
-    }
-  }
-link.init();
+"use strict";var link={init:function(){$.getJSON("/links/linklist.json",function(n){var i=document.createElement("div");i.innerHTML=n.length,document.getElementById("links-count").appendChild(i)})}};link.init();
